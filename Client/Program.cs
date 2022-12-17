c0fd5d6ba9264f0c5c0a7455a9d7b29261992be0
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-17 13:37:31 +0100

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorWebAssemblyOpenIdTest.Client
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Services.AddOidcAuthentication(options =>
+			{
+				//// https://dev-69746596.okta.com/.well-known/openid-configuration
+				options.ProviderOptions.Authority = "https://dev-69746596.okta.com/";
+				options.ProviderOptions.ClientId = "0oa7ocehkhG2vowuy5d7";
+				options.ProviderOptions.ResponseType = "code";
+				options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:4200/";
+				options.ProviderOptions.RedirectUri = "https://localhost:4200/cb";
+				options.ProviderOptions.DefaultScopes.Add("openid");
+				options.ProviderOptions.DefaultScopes.Add("profile");
+			});
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
